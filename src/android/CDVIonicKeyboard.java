271f01b02dd535a4925a8a7592593c3047cf76a0
Sylvain Dumazet <sylvain.dumazet@celsius-online.com>
2019-10-04 14:40:45 +0200

    Showing navigation bar when the keyboard is opened

--- src/android/CDVIonicKeyboard.java
+++ src/android/CDVIonicKeyboard.java
@@ -164,7 +164,7 @@ public class CDVIonicKeyboard extends CordovaPlugin {
                                 int uiOptions = window.getDecorView().getSystemUiVisibility();
                                 uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
                                 uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-                                uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                                //uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
                                 uiOptions &= ~View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
                                 uiOptions &= ~View.SYSTEM_UI_FLAG_FULLSCREEN;
                                 uiOptions &= ~View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;

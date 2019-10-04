c169a5287c64972252f5c71fb7bf0e106af5015e
Sylvain Dumazet <sylvain.dumazet@celsius-online.com>
2019-10-04 14:18:56 +0200

    Showing navigation bar when the keyboard is opened

--- src/android/CDVIonicKeyboard.java
+++ src/android/CDVIonicKeyboard.java
@@ -159,18 +159,27 @@ public class CDVIonicKeyboard extends CordovaPlugin {
                         }
 
                         private void showNavigationBar() {
+                            hideNavigationBar();
                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                                 int uiOptions = window.getDecorView().getSystemUiVisibility();
-                                uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-                                uiOptions &= ~View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                                uiOption &= ~View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                                uiOption &= ~View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+                                uiOption &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                                uiOption &= ~View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                                uiOption &= ~View.SYSTEM_UI_FLAG_FULLSCREEN;
+                                uiOption &= ~View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
                                 window.getDecorView().setSystemUiVisibility(uiOptions);
                             }
                         }
 
                         private void hideNavigationBar() {
                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                                final int uiOptions = window.getDecorView().getSystemUiVisibility()
+                                int uiOptions = window.getDecorView().getSystemUiVisibility()
+                                    |  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
                                 window.getDecorView().setSystemUiVisibility(uiOptions);
                             }
